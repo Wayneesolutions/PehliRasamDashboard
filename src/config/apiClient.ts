bd7e6417de9bd6a18e16225e83d3bc5f16c552fa
sant <santwayne123@gmail.com>
2025-04-16 17:55:15 +0530

    ui

--- src/config/apiClient.ts
+++ src/config/apiClient.ts
@@ -331,3 +331,31 @@ export const getMatchSuggestions = async (customerId: string) => {
     return res.data;
   };
   
+  
+
+ export const createEmailTemplate = async (data) => {
+  try {
+    const response = await apiClient.post('admin/createEmailTemplate',data)
+    return response?.data
+  } catch (error) {
+    return (error as Error).response?.data;
+  }
+}
+
+export const getAllEmailTemplates = async () => {
+  try {
+    const response = await apiClient.get('admin/getAllEmailTemplates')
+    return response?.data
+  } catch (error) {
+    return (error as Error).response?.data;
+  }
+}
+
+export const deleteEmailTemplate = async (id:string) => {
+  try {
+    const response = await apiClient.post('admin/deleteEmailTemplate',id)
+    return response?.data
+  } catch (error) {
+    return (error as Error).response?.data;
+  }
+}
